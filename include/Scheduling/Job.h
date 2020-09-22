8a870cc65e84eedc7ec0ee138ec1fb7b7f5ae0e7
Clement-Praxinos <clement.berthaud@praxinos.coop>
2020-09-22 13:08:24 +0200

    feat: add FJob class for actual scheduled tasks

---
+++ include/Scheduling/Job.h
@@ -0,0 +1,54 @@
+// Copyright 2018-2020 Praxinos, Inc. All Rights Reserved.
+// IDDN FR.001.250001.002.S.P.2019.000.00000
+/*
+*
+*   ULIS
+*__________________
+*
+* @file         Job.h
+* @author       Clement Berthaud
+* @brief        This file provides the declaration for the FJob class.
+* @copyright    Copyright 2018-2020 Praxinos, Inc. All Rights Reserved.
+* @license      Please refer to LICENSE.md
+*/
+#pragma once
+#include "Core/Core.h"
+
+ULIS_NAMESPACE_BEGIN
+/////////////////////////////////////////////////////
+/// @class      FJob
+/// @brief      The FJob class provides a way to store awaiting scheduled Jobs,
+///             which are the actual workers scheduled from commands in
+///             a command queue and in a pool, with all informations needed for
+///             processecing the task.
+/// @details    The FJob is used by FCommandQueue and FThreadPool and stores
+///             information about a job and its parent command which status is
+///
+///             \sa FCommandQueue
+///             \sa FThreadPool
+class ULIS_API FJob {
+public:
+    /*! Destructor */
+    ~FJob();
+
+    /*! Constructor */
+    FJob();
+
+    /*! Explicitely deleted default constructor. */
+    FJob() = delete;
+
+    /*! Explicitely deleted copy constructor. */
+    FJob( const FJob& ) = delete;
+
+    /*! Explicitely deleted move constructor. */
+    FJob( FJob&& ) = delete;
+
+    /*! Explicitely deleted copy assignment operator. */
+    FJob& operator=( const FJob& ) = delete;
+
+    /*! Explicitely deleted move assignment operator. */
+    FJob& operator=( FJob&& ) = delete;
+};
+
+ULIS_NAMESPACE_END
+
---
+++ source/Scheduling/Job.cpp
@@ -0,0 +1,28 @@
+// Copyright 2018-2020 Praxinos, Inc. All Rights Reserved.
+// IDDN FR.001.250001.002.S.P.2019.000.00000
+/*
+*
+*   ULIS
+*__________________
+*
+* @file         Job.cpp
+* @author       Clement Berthaud
+* @brief        This file provides the definition for the FJob class.
+* @copyright    Copyright 2018-2020 Praxinos, Inc. All Rights Reserved.
+* @license      Please refer to LICENSE.md
+*/
+#pragma once
+#include "Scheduling/Job.h"
+
+ULIS_NAMESPACE_BEGIN
+
+FJob::~FJob()
+{
+}
+
+FJob::FJob()
+{
+}
+
+ULIS_NAMESPACE_END
+
