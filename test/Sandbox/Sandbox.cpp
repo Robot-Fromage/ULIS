b0cf6a4b333e8bf6fc0dd231a7f994abf08ba680
Clement-Praxinos <clement.berthaud@praxinos.coop>
2020-10-10 15:53:13 +0200

    feat: add Sandbox program
    Sandbox doesn't link with ULIS in order to quicly test stuff without switching projects

---
+++ test/Sandbox/Sandbox.cpp
@@ -0,0 +1,19 @@
+// Copyright 2018-2020 Praxinos, Inc. All Rights Reserved.
+// IDDN FR.001.250001.002.S.P.2019.000.00000
+/**
+*
+*   ULIS
+*__________________
+*
+* @file         Sandbox.cpp
+* @author       Clement Berthaud
+* @brief        Sandbox program without ULIS.
+* @copyright    Copyright 2018-2020 Praxinos, Inc. All Rights Reserved.
+* @license      Please refer to LICENSE.md
+*/
+
+int
+main() {
+    return  0;
+}
+
