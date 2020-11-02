9cf61b1a259e9ddf4b7c07d634be9293bf348a41
Clement-Praxinos <clement.berthaud@praxinos.coop>
2020-11-02 15:35:52 +0100

    fix errors in BlendMT_NonSeparable_SSE_RGBA8

--- source/Blend/RGBA8/BlendMT_NonSeparable_SSE_RGBA8.cpp
+++ source/Blend/RGBA8/BlendMT_NonSeparable_SSE_RGBA8.cpp
@@ -46,11 +46,11 @@ InvokeBlendMTProcessScanline_NonSeparable_SSE_RGBA8_Subpixel(
 
     Vec4f alpha_m11, alpha_m01, alpha_m10, alpha_m00, alpha_vv0, alpha_vv1, alpha_smp;
     Vec4f smpch_m11, smpch_m01, smpch_m10, smpch_m00, smpch_vv0, smpch_vv1, smpch_smp;
-    alpha_m11 = ( notLastLine && onLeftBorder && hasLeftData )      ? *( iSrc - fmt.BPP + fmt.AID           ) / 255.f : 0.f;
-    alpha_m10 = ( hasLeftData && ( notFirstLine || hasTopData ) )   ? *( iSrc - fmt.BPP + fmt.AID - iSrcBps ) / 255.f : 0.f;
+    alpha_m11 = ( notLastLine && onLeftBorder && hasLeftData )      ? *( src - fmt.BPP + fmt.AID                    ) / 255.f : 0.f;
+    alpha_m10 = ( hasLeftData && ( notFirstLine || hasTopData ) )   ? *( src - fmt.BPP + fmt.AID - jargs->src_bps   ) / 255.f : 0.f;
     alpha_vv1 = alpha_m10 * TY + alpha_m11 * UY;
-    smpch_m11 = ( notLastLine && onLeftBorder && hasLeftData )      ? Vec4f( _mm_cvtepi32_ps( _mm_cvtepu8_epi32( _mm_loadu_si128( (const __m128i*)( iSrc - fmt.BPP            ) ) ) ) ) / 255.f : 0.f;
-    smpch_m10 = ( hasLeftData && ( notFirstLine || hasTopData ) )   ? Vec4f( _mm_cvtepi32_ps( _mm_cvtepu8_epi32( _mm_loadu_si128( (const __m128i*)( iSrc - fmt.BPP - iSrcBps  ) ) ) ) ) / 255.f : 0.f;
+    smpch_m11 = ( notLastLine && onLeftBorder && hasLeftData )      ? Vec4f( _mm_cvtepi32_ps( _mm_cvtepu8_epi32( _mm_loadu_si128( (const __m128i*)( src - fmt.BPP                   ) ) ) ) ) / 255.f : 0.f;
+    smpch_m10 = ( hasLeftData && ( notFirstLine || hasTopData ) )   ? Vec4f( _mm_cvtepi32_ps( _mm_cvtepu8_epi32( _mm_loadu_si128( (const __m128i*)( src - fmt.BPP - jargs->src_bps  ) ) ) ) ) / 255.f : 0.f;
     smpch_vv1 = ( smpch_m10 * alpha_m10 ) * TY + ( smpch_m11 * alpha_m11 )  * UY;
 
     for( int x = 0; x < cargs->backdropWorkingRect.w; ++x ) {
@@ -61,23 +61,23 @@ InvokeBlendMTProcessScanline_NonSeparable_SSE_RGBA8_Subpixel(
         smpch_m00 = smpch_m10;
         smpch_m01 = smpch_m11;
         smpch_vv0 = smpch_vv1;
-        alpha_m11 = ( notLastCol && notLastLine )                     ? *( iSrc + fmt.AID             ) / 255.f : 0.f;
-        alpha_m10 = ( notLastCol && ( notFirstLine || hasTopData ) )  ? *( iSrc + fmt.AID - iSrcBps   ) / 255.f : 0.f;
+        alpha_m11 = ( notLastCol && notLastLine )                     ? *( src + fmt.AID                    ) / 255.f : 0.f;
+        alpha_m10 = ( notLastCol && ( notFirstLine || hasTopData ) )  ? *( src + fmt.AID - jargs->src_bps   ) / 255.f : 0.f;
         alpha_vv1 = alpha_m10 * TY + alpha_m11 * UY;
         alpha_smp = alpha_vv0 * TX + alpha_vv1 * UX;
-        smpch_m11 = ( notLastCol && notLastLine )                     ? Vec4f( _mm_cvtepi32_ps( _mm_cvtepu8_epi32( _mm_loadu_si128( (const __m128i*)( iSrc              ) ) ) ) ) / 255.f : 0.f;
-        smpch_m10 = ( notLastCol && ( notFirstLine || hasTopData ) )  ? Vec4f( _mm_cvtepi32_ps( _mm_cvtepu8_epi32( _mm_loadu_si128( (const __m128i*)( iSrc - iSrcBps    ) ) ) ) ) / 255.f : 0.f;
+        smpch_m11 = ( notLastCol && notLastLine )                     ? Vec4f( _mm_cvtepi32_ps( _mm_cvtepu8_epi32( _mm_loadu_si128( (const __m128i*)( src                   ) ) ) ) ) / 255.f : 0.f;
+        smpch_m10 = ( notLastCol && ( notFirstLine || hasTopData ) )  ? Vec4f( _mm_cvtepi32_ps( _mm_cvtepu8_epi32( _mm_loadu_si128( (const __m128i*)( src - jargs->src_bps  ) ) ) ) ) / 255.f : 0.f;
         smpch_vv1 = ( smpch_m10 * alpha_m10 ) * TY + ( smpch_m11 * alpha_m11 )  * UY;
-        smpch_smp = lookup4( iIDT, select( alpha_smp == 0.f, 0.f, ( smpch_vv0 * TX + smpch_vv1 * UX ) / alpha_smp ) );
+        smpch_smp = lookup4( jargs->idt, select( alpha_smp == 0.f, 0.f, ( smpch_vv0 * TX + smpch_vv1 * UX ) / alpha_smp ) );
 
-        Vec4f alpha_bdp     = *( iBdp + fmt.AID ) / 255.f;
+        Vec4f alpha_bdp     = *( bdp + fmt.AID ) / 255.f;
         Vec4f alpha_src     = alpha_smp * cargs->opacity;
         Vec4f alpha_comp    = AlphaNormalSSEF( alpha_src, alpha_bdp );
         Vec4f var           = select( alpha_comp == 0.f, 0.f, alpha_src / alpha_comp );
         Vec4f alpha_result;
         ULIS_ASSIGN_ALPHASSEF( cargs->alphaMode, alpha_result, alpha_src, alpha_bdp );
 
-        Vec4f bdp_chan = lookup4( iIDT, Vec4f( _mm_cvtepi32_ps( _mm_cvtepu8_epi32( _mm_loadu_si128( (const __m128i*)( iBdp ) ) ) ) ) / 255.f );
+        Vec4f bdp_chan = lookup4( jargs->idt, Vec4f( _mm_cvtepi32_ps( _mm_cvtepu8_epi32( _mm_loadu_si128( (const __m128i*)( bdp ) ) ) ) ) / 255.f );
         smpch_smp.insert( 3, 0.f );
         bdp_chan.insert( 3, 0.f );
         Vec4f res_chan;
@@ -85,14 +85,14 @@ InvokeBlendMTProcessScanline_NonSeparable_SSE_RGBA8_Subpixel(
         ULIS_SWITCH_FOR_ALL_DO( cargs->blendingMode, ULIS_FOR_ALL_NONSEPARABLE_BM_DO, TMP_ASSIGN, 0, 0, 0 )
         #undef TMP_ASSIGN
 
-        res_chan = lookup4( iIDT, res_chan );
+        res_chan = lookup4( jargs->idt, res_chan );
         auto _pack = _mm_cvtps_epi32( res_chan );
         _pack = _mm_packus_epi32( _pack, _pack );
         _pack = _mm_packus_epi16( _pack, _pack );
-        *( uint32* )iBdp = static_cast< uint32 >( _mm_cvtsi128_si32( _pack ) );
-        *( iBdp + fmt.AID ) = uint8( alpha_result[0] * 0xFF );
-        iSrc += 4;
-        iBdp += 4;
+        *( uint32* )bdp = static_cast< uint32 >( _mm_cvtsi128_si32( _pack ) );
+        *( bdp + fmt.AID ) = uint8( alpha_result[0] * 0xFF );
+        src += 4;
+        bdp += 4;
     }
 }
 
@@ -116,15 +116,15 @@ InvokeBlendMTProcessScanline_NonSeparable_SSE_RGBA8(
     uint8*       ULIS_RESTRICT  bdp = jargs->bdp;
 
     for( int x = 0; x < cargs->backdropWorkingRect.w; ++x ) {
-        ufloat alpha_bdp    = iBdp[fmt.AID] / 255.f;
-        ufloat alpha_src    = ( iSrc[fmt.AID] / 255.f ) * cargs->opacity;
+        ufloat alpha_bdp    = bdp[fmt.AID] / 255.f;
+        ufloat alpha_src    = ( src[fmt.AID] / 255.f ) * cargs->opacity;
         ufloat alpha_comp   = AlphaNormalF( alpha_src, alpha_bdp );
         ufloat var          = alpha_comp == 0.f ? 0.f : alpha_src / alpha_comp;
         ufloat alpha_result;
         ULIS_ASSIGN_ALPHAF( cargs->alphaMode, alpha_result, alpha_src, alpha_bdp );
 
-        Vec4f src_chan = lookup4( iIDT, Vec4f( _mm_cvtepi32_ps( _mm_cvtepu8_epi32( _mm_loadu_si128( (const __m128i*)( iSrc ) ) ) ) ) / 255.f );
-        Vec4f bdp_chan = lookup4( iIDT, Vec4f( _mm_cvtepi32_ps( _mm_cvtepu8_epi32( _mm_loadu_si128( (const __m128i*)( iBdp ) ) ) ) ) / 255.f );
+        Vec4f src_chan = lookup4( jargs->idt, Vec4f( _mm_cvtepi32_ps( _mm_cvtepu8_epi32( _mm_loadu_si128( (const __m128i*)( src ) ) ) ) ) / 255.f );
+        Vec4f bdp_chan = lookup4( jargs->idt, Vec4f( _mm_cvtepi32_ps( _mm_cvtepu8_epi32( _mm_loadu_si128( (const __m128i*)( bdp ) ) ) ) ) / 255.f );
         src_chan.insert( 3, 0.f );
         bdp_chan.insert( 3, 0.f );
         Vec4f res_chan;
@@ -132,14 +132,14 @@ InvokeBlendMTProcessScanline_NonSeparable_SSE_RGBA8(
         ULIS_SWITCH_FOR_ALL_DO( cargs->blendingMode, ULIS_FOR_ALL_NONSEPARABLE_BM_DO, TMP_ASSIGN, 0, 0, 0 )
         #undef TMP_ASSIGN
 
-        res_chan = lookup4( iIDT, res_chan );
+        res_chan = lookup4( jargs->idt, res_chan );
         auto _pack = _mm_cvtps_epi32( res_chan );
         _pack = _mm_packus_epi32( _pack, _pack );
         _pack = _mm_packus_epi16( _pack, _pack );
-        *( uint32* )iBdp = static_cast< uint32 >( _mm_cvtsi128_si32( _pack ) );
-        *( iBdp + fmt.AID ) = uint8( alpha_result * 0xFF );
-        iSrc += 4;
-        iBdp += 4;
+        *( uint32* )bdp = static_cast< uint32 >( _mm_cvtsi128_si32( _pack ) );
+        *( bdp + fmt.AID ) = uint8( alpha_result * 0xFF );
+        src += 4;
+        bdp += 4;
     }
 }
 
