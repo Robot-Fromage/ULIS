46506fddce482027fa2fbb2e69723be404da5d8f
Clement-Praxinos <clement.berthaud@praxinos.coop>
2020-11-17 11:52:11 +0100

    feat: add basis for Copy helper

---
+++ source/Copy/CopyHelpers.h
@@ -0,0 +1,19 @@
+// IDDN FR.001.250001.004.S.X.2019.000.00000
+// ULIS is subject to copyright laws and is the legal and intellectual property of Praxinos,Inc
+/*
+*
+*   ULIS
+*__________________
+*
+* @file         CopyHelpers.h
+* @author       Clement Berthaud
+* @brief        This file provides some helper macros and functions for copying.
+* @copyright    Copyright 2018-2020 Praxinos, Inc. All Rights Reserved.
+* @license      Please refer to LICENSE.md
+*/
+#pragma once
+#include "Core/Core.h"
+
+ULIS_NAMESPACE_BEGIN
+ULIS_NAMESPACE_END
+
