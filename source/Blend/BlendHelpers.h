d6015498315e3ae9d0813b4a7c034c73bc403c3d
Clement-Praxinos <clement.berthaud@praxinos.coop>
2020-11-18 16:16:28 +0100

    fix: set gBayerMatrix in ULIS Namespace in Blend Helpers

--- source/Blend/BlendHelpers.h
+++ source/Blend/BlendHelpers.h
@@ -19,18 +19,6 @@
 #include "Scheduling/Job.h"
 
 /////////////////////////////////////////////////////
-// Static Values Helpers for Misc Compositing Operations
-static const float gBayer8x8Matrix[8][8] = {
-    { 0.015625f, 0.765625f, 0.203125f, 0.953125f, 0.0625f, 0.8125f, 0.25f, 1.0f          },
-    { 0.515625f, 0.265625f, 0.703125f, 0.453125f, 0.5625f, 0.3125f, 0.75f, 0.5f          },
-    { 0.140625f, 0.890625f, 0.078125f, 0.828125f, 0.1875f, 0.9375f, 0.125f, 0.875f       },
-    { 0.640625f, 0.390625f, 0.578125f, 0.328125f, 0.6875f, 0.4375f, 0.625f, 0.375f       },
-    { 0.046875f, 0.796875f, 0.234375f, 0.984375f, 0.03125f, 0.78125f, 0.21875f, 0.96875f },
-    { 0.546875f, 0.296875f, 0.734375f, 0.484375f, 0.53125f, 0.28125f, 0.71875f, 0.46875f },
-    { 0.171875f, 0.921875f, 0.109375f, 0.859375f, 0.15625f, 0.90625f, 0.09375f, 0.84375f },
-    { 0.671875f, 0.421875f, 0.609375f, 0.359375f, 0.65625f, 0.40625f, 0.59375f, 0.34375f } };
-
-/////////////////////////////////////////////////////
 // Macro Helpers for Redundant Compositing Operations
 // TODO: this is quite clumsy at the moment, clean this by defining better
 // macros for ASSIGN and small helper functions, possibly inline.
@@ -62,6 +50,18 @@ static const float gBayer8x8Matrix[8][8] = {
 #define ULIS_ASSIGN_ALPHAAVXF( iAlphaMode, iTarget, iSrc, iBdp )   ULIS_SWITCH_FOR_ALL_DO( iAlphaMode, ULIS_FOR_ALL_AM_DO, ULIS_ACTION_ASSIGN_ALPHAAVXF, iTarget, iSrc, iBdp )
 
 ULIS_NAMESPACE_BEGIN
+/////////////////////////////////////////////////////
+// Static Values Helpers for Misc Compositing Operations
+static const float gBayer8x8Matrix[8][8] = {
+    { 0.015625f, 0.765625f, 0.203125f, 0.953125f, 0.0625f, 0.8125f, 0.25f, 1.0f          },
+    { 0.515625f, 0.265625f, 0.703125f, 0.453125f, 0.5625f, 0.3125f, 0.75f, 0.5f          },
+    { 0.140625f, 0.890625f, 0.078125f, 0.828125f, 0.1875f, 0.9375f, 0.125f, 0.875f       },
+    { 0.640625f, 0.390625f, 0.578125f, 0.328125f, 0.6875f, 0.4375f, 0.625f, 0.375f       },
+    { 0.046875f, 0.796875f, 0.234375f, 0.984375f, 0.03125f, 0.78125f, 0.21875f, 0.96875f },
+    { 0.546875f, 0.296875f, 0.734375f, 0.484375f, 0.53125f, 0.28125f, 0.71875f, 0.46875f },
+    { 0.171875f, 0.921875f, 0.109375f, 0.859375f, 0.15625f, 0.90625f, 0.09375f, 0.84375f },
+    { 0.671875f, 0.421875f, 0.609375f, 0.359375f, 0.65625f, 0.40625f, 0.59375f, 0.34375f } };
+
 /////////////////////////////////////////////////////
 // Helper for Redundant Job building and scheduling operations
 ULIS_FORCEINLINE
