ac2c6314ac98794d62a980bedb5d525b84beed49
Clement-Praxinos <clement.berthaud@praxinos.coop>
2020-12-02 12:11:46 +0100

    reset sandbox

--- test/Sandbox/Sandbox.cpp
+++ test/Sandbox/Sandbox.cpp
@@ -17,85 +17,8 @@ typedef uint8_t uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
 
-class ICommandArgs {
-public:
-    virtual ~ICommandArgs() {};
-};
-
-
-class FClearCommandArgs final
-    : public ICommandArgs
-{
-public:
-    ~FClearCommandArgs() override {};
-    FClearCommandArgs() {};
-
-    uint8 dst;
-    uint8 size;
-};
-
-
-class IJobArgs {
-public:
-    virtual ~IJobArgs() {};
-};
-
-
-class FClearJobArgs final
-    : public IJobArgs
-{
-public:
-    ~FClearJobArgs() override {};
-    FClearJobArgs() {};
-
-    uint8 dst;
-    uint8 size;
-};
-
-void
-InvokeClear(
-      const FClearJobArgs* jargs
-    , const FClearCommandArgs* cargs
-)
-{
-    const volatile auto test = 0;
-}
-
-static
-void
-BuildClearJob(
-      const uint32 iNumJobs
-    , const uint32 iNumTasksPerJob
-    , FClearJobArgs& iJob
-)
-{
-    const volatile auto test = 0;
-}
-
-template< typename T, typename U, void (*TDelegateInvoke)( const T*, const U* ), void (*TDelegateBuild)( const uint32, const uint32, T& ) >
-static
-void
-RangeBaseSchedulingBuildJobs(
-      const uint32 iNumJobs
-    , const uint32 iNumTasksPerJob
-)
-{
-    for( int i = 0; i < iNumJobs; ++i )
-    {
-        uint8* buf = new uint8[ iNumTasksPerJob * sizeof( T ) ];
-        T* jargs = reinterpret_cast< T* >( buf );
-
-        for( int i = 0; i < iNumTasksPerJob; ++i ) {
-            new ( buf ) T();
-            TDelegateBuild( iNumJobs, iNumTasksPerJob, jargs[i] );
-        }
-
-    }
-}
-
 int
 main() {
-    RangeBaseSchedulingBuildJobs< FClearJobArgs, FClearCommandArgs, InvokeClear, BuildClearJob >( 2, 1 );
     return  0;
 }
 
