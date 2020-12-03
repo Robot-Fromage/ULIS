200293c154c8afc76995ca523e423c264b40b3f8
Clement-Praxinos <clement.berthaud@praxinos.coop>
2020-12-03 13:16:06 +0100

    feat: apply simpler async scheduling to clear api

--- source/Clear/Clear.h
+++ source/Clear/Clear.h
@@ -14,46 +14,92 @@
 #pragma once
 #include "Core/Core.h"
 #include "Dispatch/Dispatcher.h"
+#include "Image/Block.h"
 #include "Math/Geometry/Rectangle.h"
+#include "Scheduling/RangeBasedPolicyScheduler.h"
 #include "Scheduling/ScheduleArgs.h"
+#include "Scheduling/SimpleBuffer.h"
 
 ULIS_NAMESPACE_BEGIN
 /////////////////////////////////////////////////////
-// FClearCommandArgs
-class FClearCommandArgs final
-    : public ICommandArgs
+// Invocations
+//--------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------- AVX
+#ifdef ULIS_COMPILETIME_AVX2_SUPPORT
+static
+void
+InvokeClearMT_AX2(
+      const FSimpleBufferJobArgs* jargs
+    , const FSimpleBufferJobArgs* cargs
+)
 {
-public:
-    ~FClearCommandArgs() override {}
+    __m256i* ULIS_RESTRICT dst = reinterpret_cast< __m256i* >( jargs->dst );
+    int64 index = 0;
+    for( index = 0; index < jargs->size - 32; index += 32 ) {
+        _mm256_storeu_si256( dst, _mm256_setzero_si256() );
+        ++dst;
+    }
+    // Remaining unaligned scanline end: avoid concurrent write on 256 bit with avx and perform a memset instead
+    memset( dst, 0, jargs->size - index );
+}
+#endif // ULIS_COMPILETIME_AVX2_SUPPORT
 
-    FClearCommandArgs(
-          FBlock& iBlock
-        , const FRectI& iRect
-    )
-        : ICommandArgs()
-        , block( iBlock )
-        , rect( iRect )
-        {}
+//--------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------- SSE
+#ifdef ULIS_COMPILETIME_SSE42_SUPPORT
+static
+void
+InvokeClearMT_SSE4_2(
+      const FSimpleBufferJobArgs* jargs
+    , const FSimpleBufferJobArgs* cargs
+)
+{
+    __m128i* ULIS_RESTRICT dst = reinterpret_cast< __m128i* >( jargs->dst );
+    int64 index = 0;
+    for( index = 0; index < jargs->size - 16; index += 16 ) {
+        _mm_storeu_si128( dst, _mm_setzero_si128() );
+        ++dst;
+    }
+    // Remaining unaligned scanline end: avoid concurrent write on 128 bit with SSE and perform a memset instead
+    memset( dst, 0, jargs->size - index );
+}
+#endif // __SE4_2__
 
-    FBlock& block;
-    const FRectI rect;
-};
-
-/////////////////////////////////////////////////////
-// Scheduler
-ULIS_DECLARE_COMMAND_SCHEDULER( ScheduleClearMT_AX2 );
-ULIS_DECLARE_COMMAND_SCHEDULER( ScheduleClearMT_SSE4_2 );
-ULIS_DECLARE_COMMAND_SCHEDULER( ScheduleClearMT_MEM );
+//--------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------- MEM
+static
+void
+InvokeClearMT_MEM(
+      const FSimpleBufferJobArgs* jargs
+    , const FSimpleBufferJobArgs* cargs
+)
+{
+    // Full scanline width instead of many BPP clears
+    memset( jargs->dst, 0, jargs->size );
+}
 
 /////////////////////////////////////////////////////
 // Dispatch
 ULIS_DECLARE_DISPATCHER( FDispatchedClearInvocationSchedulerSelector )
 ULIS_DEFINE_DISPATCHER_GENERIC_GROUP(
       FDispatchedClearInvocationSchedulerSelector
-    , &ScheduleClearMT_AX2
-    , &ScheduleClearMT_SSE4_2
-    , &ScheduleClearMT_MEM
+    , &ScheduleSimpleBufferJobs<
+          FSimpleBufferJobArgs
+        , FFillPreserveAlphaCommandArgs
+        , &InvokeClearMT_AX2< T >
+        >
+    , &ScheduleSimpleBufferJobs<
+          FSimpleBufferJobArgs
+        , FFillPreserveAlphaCommandArgs
+        , &InvokeClearMT_SSE< T >
+        >
+    , &ScheduleSimpleBufferJobs<
+          FSimpleBufferJobArgs
+        , FFillPreserveAlphaCommandArgs
+        , &InvokeClearMT_MEM< T >
+        >
 )
+ULIS_DISPATCHER_NO_SPECIALIZATION_DEFINITION( FDispatchedClearInvocationSchedulerSelector )
 
 ULIS_NAMESPACE_END
 
