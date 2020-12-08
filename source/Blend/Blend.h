a4f0d0b3ef3b93a62a1e472e0d57ea687f24ef15
Clement-Praxinos <clement.berthaud@praxinos.coop>
2020-12-08 15:59:04 +0100

    wip: restore Blend files for simpler API

---
+++ source/Blend/Blend.h
@@ -0,0 +1,23 @@
+// IDDN FR.001.250001.004.S.X.2019.000.00000
+// ULIS is subject to copyright laws and is the legal and intellectual property of Praxinos,Inc
+/*
+*   ULIS
+*__________________
+* @file         Blend.h
+* @author       Clement Berthaud
+* @brief        This file provides the declaration for the Blend API.
+* @copyright    Copyright 2018-2020 Praxinos, Inc. All Rights Reserved.
+* @license      Please refer to LICENSE.md
+*/
+#pragma once
+#include "Core/Core.h"
+#include "Conv/Conv.h"
+#include "Dispatch/Dispatcher.h"
+#include "Math/Geometry/Rectangle.h"
+#include "Scheduling/ScheduleArgs.h"
+#include "Scheduling/DualBufferArgs.h"
+
+ULIS_NAMESPACE_BEGIN
+
+ULIS_NAMESPACE_END
+
