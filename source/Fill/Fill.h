ece30212f3106d910c7a4d5ab4d605edd6740699
Clement-Praxinos <clement.berthaud@praxinos.coop>
2020-12-10 14:50:34 +0100

    fix: bug in strides in FPA

--- source/Fill/Fill.h
+++ source/Fill/Fill.h
@@ -74,13 +74,12 @@ InvokeFillPreserveAlphaMT_MEM_Generic(
 {
     const uint8* src = cargs->color.Bits();
     const FFormatMetrics& fmt = cargs->block.Format();
-    const uint8 stride = cargs->block.BytesPerPixel();
-    uint8* ULIS_RESTRICT dst = jargs->dst;
+    T* ULIS_RESTRICT dst = reinterpret_cast< T* >( jargs->dst );
     for( uint32 i = 0; i < jargs->size; ++i ) {
-        const T alpha = dst[ iFmt.AID ];
-        memcpy( dst, src, stride );
+        const T alpha = dst[ fmt.AID ];
+        memcpy( dst, src, fmt.BPP );
         dst[ fmt.AID ] = alpha;
-        dst += stride;
+        dst += fmt.SPP;
     }
 }
 
