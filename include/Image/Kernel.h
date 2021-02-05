762da3d50f381fa79397edd8d5e82e61a35d47ab
Clement-Praxinos <clement.berthaud@praxinos.coop>
2021-02-05 11:54:44 +0100

    feat: imp FKernel ( needs testing )

---
+++ include/Image/Kernel.h
@@ -0,0 +1,75 @@
+// IDDN FR.001.250001.004.S.X.2019.000.00000
+// ULIS is subject to copyright laws and is the legal and intellectual property of Praxinos,Inc
+/*
+*   ULIS
+*__________________
+* @file         Kernel.h
+* @author       Clement Berthaud
+* @brief        This file provides the declaration for the FKernel class.
+* @copyright    Copyright 2018-2021 Praxinos, Inc. All Rights Reserved.
+* @license      Please refer to LICENSE.md
+*/
+#pragma once
+#include "Core/Core.h"
+#include "Image/Block.h"
+
+ULIS_NAMESPACE_BEGIN
+/////////////////////////////////////////////////////
+/// @class      FConvolutionKernel
+/// @brief      The FKernel class provides a mean of storing and manipulating convolution kernels
+/// @details    The kernel is actually a block in Gray floating point format.
+///             kernel data lives on CPU, kernel can also be used as matrix.
+class ULIS_API FKernel : public FBlock
+{
+public:
+    typedef FKernel tSelf;
+    typedef FBlock  tSuperClass;
+
+public:
+    // Construction / Destruction
+    ~FKernel();
+    FKernel( const FVec2I& iSize, float iValue = 0.f );
+    FKernel( const FVec2I& iSize, std::initializer_list< float > iNums );
+    FKernel( const FKernel& );
+    FKernel( FKernel&& );
+    FKernel& operator=( const FKernel& );
+    FKernel& operator=( FKernel&& );
+
+public:
+    // Public API
+    void  Set( std::initializer_list< float > iNums );
+    float At( int iX, int iY ) const;
+    float At( FVec2I iPoint ) const;
+    void SetAt( int iX, int iY, float iValue );
+    void SetAt( FVec2I iPoint, float iValue );
+    void Clear();
+    void Fill( float iValue );
+    void SetZeroes();
+    void SetOnes();
+    float Sum() const;
+    void Add( float iValue );
+    void Mul( float iValue );
+    void Normalize();
+    bool IsNormalized() const;
+    void FlipX();
+    void FlipY();
+    void Rotate90CW();
+    void Rotate90CCW();
+    void Rotate180();
+    FKernel Normalized() const;
+    FKernel FlippedX() const;
+    FKernel FlippedY() const;
+    FKernel Rotated90CW() const;
+    FKernel Rotated90CCW() const;
+    FKernel Rotated180() const;
+    const FVec2I Size() const;
+    const FVec2I& Pivot() const;
+    void SetPivot( const FVec2I& iPivot );
+
+private:
+    // Private Data Members
+    FVec2I mPivot;
+};
+
+ULIS_NAMESPACE_END
+
