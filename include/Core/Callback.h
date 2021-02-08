dc7e7bdc8ce8ddf240d21126c9b5f46920a9a4ad
Clement-Praxinos <clement.berthaud@praxinos.coop>
2021-02-08 17:29:30 +0100

    wip: add TCallback with capture ability for FEvent callback

--- include/Core/Callback.h
+++ include/Core/Callback.h
@@ -11,6 +11,7 @@
 */
 #pragma once
 #include "Core/Core.h"
+#include <functional>
 
 ULIS_NAMESPACE_BEGIN
 
@@ -57,12 +58,51 @@ private:
     void* mInfo;
 };
 
+#pragma warning(push)
+#pragma warning(disable : 4251) // Shut warning C4251 dll export of stl classes
+template< typename R, typename ... Ts >
+class TCallback2
+{
+public:
+    /*! Typedef tFptr for the internal function pointer type. */
+    typedef std::function< R ( Ts ... ) > tFptr;
+
+    /*! Default null constructor. */
+    TCallback2()
+        : mFptr( nullptr )
+    {}
+
+    /*! Constructor from function pointer and optional extra info. */
+    TCallback2( tFptr iFptr )
+        : mFptr( iFptr )
+    {}
+
+    /*!
+        Call the function pointer only if set.
+        No return value because we can't ensure a generic default return value if not bound.
+    */
+    ULIS_FORCEINLINE void ExecuteIfBound( Ts ... args ) const {
+        mFptr( args ... );
+    }
+
+    /*!
+        Call the function pointer without checking if it is set.
+    */
+    ULIS_FORCEINLINE R Execute( Ts ... args ) const {
+        return  mFptr( args ... );
+    }
+
+private:
+    tFptr mFptr;
+};
+#pragma warning(pop)
+
 typedef TCallback< void, uint8* /* iData */ > FOnCleanupData;
 typedef TCallback< void, const FBlock* /* iBlock */, const FRectI* /* iRects */, const uint32 /* iNumRects */ > FOnInvalidBlock;
-typedef TCallback< void, const FRectI& /* iEvent */ > FOnEventComplete;
+typedef TCallback2< void, const FRectI& /* iEvent */ > FOnEventComplete;
 template class ULIS_API TCallback< void, uint8* >;
 template class ULIS_API TCallback< void, const FBlock*, const FRectI*, const uint32 >;
-template class ULIS_API TCallback< void, const FRectI& >;
+template class ULIS_API TCallback2< void, const FRectI& >;
 
 #ifdef ULIS_FEATURE_GPU_ENABLED
 typedef TCallback< void, const FTexture* /* iTexture */, const FRectI* /* iRects */, const uint32 /* iNumRects */ > FOnInvalidTexture;
