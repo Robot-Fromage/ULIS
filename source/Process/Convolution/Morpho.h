3c90d9e86f476bdc7d1367893ee6e0ff86a17b6f
Clement-Praxinos <clement.berthaud@praxinos.coop>
2021-02-09 10:50:16 +0100

    fix: morpho imp

--- source/Process/Convolution/Morpho.h
+++ source/Process/Convolution/Morpho.h
@@ -51,6 +51,54 @@ InvokeMorphoMT_MEM_Generic(
     , const FMorphoCommandArgs* cargs
 )
 {
+    const FFormatMetrics&       fmt = cargs->src.FormatMetrics();
+    uint8*       ULIS_RESTRICT  dst = jargs->dst;
+    bool* match = new bool[fmt.SPP];
+
+    // StructuringElement ( blank = any )
+    //   ___________
+    //  |   |   |   |
+    //  |   | 0 | 0 |
+    //  |___|___|___|
+    //  |   |   |   |
+    //  | 1 | 1 | 0 |
+    //  |___|___|___|
+    //  |   |   |   |
+    //  |   | 1 |   |
+    //  |___|___|___|
+    // match structure for channel ? -> OnSame/OnDiff -> Keep / SetOne / SetZero / Compute?
+    for( int x = 0; x < cargs->dstRect.w; ++x ) {
+        for( int i = 0; i < cargs->kernel.Width(); ++i ) {
+            for( int j = 0; j < cargs->kernel.Width(); ++j ) {
+                memset( match, 1, fmt.BPP );
+                int src_x = x + i - cargs->kernel.Pivot().x;
+                int src_y = jargs->line + j - cargs->kernel.Pivot().y;
+                FColor color = cargs->src.Sample( src_x, src_y );
+                eMorphologicalElementValue mpel = cargs->kernel.At( i, j );
+                if( mpel == MpE_Any )
+                    continue;
+                for( int k = 0; k < fmt.SPP; ++k ) {
+                    T binary = ( *( (T*)( color.Bits() + k ) ) ) > MinType< T >() ? MaxType< T >() : MinType< T >();
+                    bool A = binary == MinType< T >();
+                    bool B = mpel == MpE_Zero;
+                    bool C = binary == MaxType< T >();
+                    bool D = mpel == MpE_One;
+                    if( ( ( !A || !B ) && ( !C || !D ) ) ) {
+                        match[k] = false;
+                    }
+                }
+            }
+        }
+        for( int k = 0; k < fmt.SPP; ++k ) {
+            if( match[k] ) {
+                // Default On Match Set One
+                ( *( (T*)( dst + k ) ) ) = MaxType< T >();
+            }
+        }
+        dst += fmt.BPP;
+    }
+
+    delete [] match;
 }
 
 /////////////////////////////////////////////////////
