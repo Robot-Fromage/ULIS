8d0f062791eaecb1332f52f93a3958c7b2108c40
Clement-Praxinos <clement.berthaud@praxinos.coop>
2021-02-24 09:18:45 +0100

    fix: Blend Showcase example with FFont thread constraint

--- example/BlendShowcase/BlendShowcase.cpp
+++ example/BlendShowcase/BlendShowcase.cpp
@@ -9,7 +9,7 @@
 * @copyright    Copyright 2018-2021 Praxinos, Inc. All Rights Reserved.
 * @license      Please refer to LICENSE.md
 */
-#include <ULIS3>
+#include <ULIS>
 
 #include <QApplication>
 #include <QWidget>
@@ -19,66 +19,87 @@
 
 #include <codecvt>
 
-using namespace ::ul3;
+using namespace ::ULIS;
 
 int
 main( int argc, char *argv[] ) {
-    FThreadPool* threadPool = XCreateThreadPool();
-    FHardwareMetrics host = FHardwareMetrics::Detect();
+    FThreadPool pool;
+    FCommandQueue queue( pool );
+    eFormat fmt = Format_RGBA8;
+    FContext ctx( queue, fmt, PerformanceIntent_Max );
+    FHardwareMetrics hw;
+    FSchedulePolicy policy_cache_efficient( ScheduleRun_Multi,ScheduleMode_Chunks, ScheduleParameter_Length, hw.L1CacheSize() );
+    FSchedulePolicy policy_mono_chunk( ScheduleRun_Mono, ScheduleMode_Chunks, ScheduleParameter_Count, 1 );
+    FSchedulePolicy policy_multi_scanlines( ScheduleRun_Multi, ScheduleMode_Scanlines );
+    FSchedulePolicy policy_mono_scanlines( ScheduleRun_Mono, ScheduleMode_Scanlines );
 
-    std::string pathBase = "C:/Users/PRAXINOS/Documents/work/base_160.png";
-    std::string pathOver = "C:/Users/PRAXINOS/Documents/work/over_160.png";
+    // Create both "hollow" blocks Base and Over.
+    FBlock blockBase;
+    FBlock blockOver;
+    {
+        // Collect hard-coded paths to images.
+        std::string pathBase = "C:/Users/PRAXINOS/Documents/work/base_160.png";
+        std::string pathOver = "C:/Users/PRAXINOS/Documents/work/over_160.png";
+        // Load from file into blocks
+        ulError err;
+        err = ctx.XLoadBlockFromDisk( blockBase, pathBase );
+        ULIS_ASSERT( !err, "Load failed" );
+        err = ctx.XLoadBlockFromDisk( blockOver, pathOver );
+        ULIS_ASSERT( !err, "Load failed" );
+        ULIS_ASSERT( blockBase.Format() == fmt, "Bad format assumption." );
+        ULIS_ASSERT( blockOver.Format() == fmt, "Bad format assumption." );
+        ctx.Finish();
+    }
 
-    uint32 perfIntentLoad   = ULIS_PERF_MT | ULIS_PERF_SSE42 | ULIS_PERF_AVX2;
-    uint32 perfIntentFill   = ULIS_PERF_SSE42 | ULIS_PERF_AVX2;
-    uint32 perfIntentCopy   = ULIS_PERF_SSE42 | ULIS_PERF_AVX2;
-    uint32 perfIntentBlend  = ULIS_PERF_SSE42 | ULIS_PERF_AVX2;
-    uint32 perfIntentText   = 0;
-    FBlock* blockBase = XLoadFromFile( threadPool, ULIS_NONBLOCKING, perfIntentLoad, host, ULIS_NOCB, pathBase, ULIS_FORMAT_RGBA8 );
-    FBlock* blockOver = XLoadFromFile( threadPool, ULIS_NONBLOCKING, perfIntentLoad, host, ULIS_NOCB, pathOver, ULIS_FORMAT_RGBA8 );
-    Fence( *threadPool );
-
-    FRectI srcRect = blockBase->Rect();
+    FRectI srcRect = blockBase.Rect();
     int w = srcRect.w * 8;
     int h = srcRect.h * 5;
 
     int shadeW = srcRect.w;
     int shadeH = 20;
-    FBlock* blockCanvas = new  FBlock( w, h, ULIS_FORMAT_RGBA8 );
-    FBlock* blockShade = new  FBlock( shadeW, shadeH, ULIS_FORMAT_RGBA8 );
-    FRectI shadeRect = blockBase->Rect();
-    FColor black( ULIS_FORMAT_RGBA8, { 0, 0, 0, 255 } );
-    FColor white( ULIS_FORMAT_RGBA8, { 255, 255, 255, 255 } );
-    Fill( threadPool, ULIS_NONBLOCKING, ULIS_PERF_AVX2, host, ULIS_NOCB, blockShade, black, shadeRect );
+    FBlock blockShade( shadeW, shadeH, fmt );
+    FColor black = FColor::RGBA8( 0, 0, 0 );
+    ctx.Fill( blockShade, blockShade.Rect(), black, policy_cache_efficient );
+    ctx.Flush();
 
-    FFontEngine FontEngine;
-    FFontRegistry fontRegistry( FontEngine );
-    FFont font( fontRegistry, "Segoe UI", "Light" );
+    FBlock blockCanvas( w, h, fmt );
+    FRectI shadeRect = blockBase.Rect();
+    FColor white = FColor::RGBA8( 255, 255, 255 );
 
-    for( int i = 0; i < NUM_BLENDING_MODES; ++i ) {
+    FFontEngine fontEngine;
+    FFont font( fontEngine, "Segoe UI", "Light" );
+
+    for( int i = 0; i < NumBlendingModes; ++i ) {
         int x = ( i % 8 ) * srcRect.w;
         int y = ( i / 8 ) * srcRect.h;
-        Copy(   threadPool, ULIS_BLOCKING, perfIntentCopy, host, ULIS_NOCB, blockBase, blockCanvas, srcRect, FVec2I( x, y ) );
-        Blend(  threadPool, ULIS_BLOCKING, perfIntentBlend, host, ULIS_NOCB, blockOver, blockCanvas, srcRect, FVec2F( x, y ), ULIS_NOAA, static_cast< eBlendMode >( i ), AM_NORMAL, 0.5f );
-        Blend(  threadPool, ULIS_BLOCKING, perfIntentBlend, host, ULIS_NOCB, blockShade, blockCanvas, shadeRect, FVec2F( x, y + srcRect.h - shadeH ), ULIS_NOAA, BM_NORMAL, AM_NORMAL, 0.5f );
-        Blend(  threadPool, ULIS_BLOCKING, perfIntentBlend, host, ULIS_NOCB, blockShade, blockCanvas, shadeRect, FVec2F( x, y + srcRect.h - shadeH ), ULIS_NOAA, BM_BAYERDITHER8x8, AM_NORMAL, 0.5f );
-        std::string bm = kwBlendingMode[i];
+        FEvent event_copy;
+        ctx.Copy( blockBase, blockCanvas, srcRect, FVec2I( x, y ), policy_mono_chunk, 0, nullptr, &event_copy );
+        ctx.Flush();
+        FEvent event_blend0;
+        FEvent event_blend1;
+        ctx.Blend( blockOver, blockCanvas, srcRect, FVec2I( x, y ), static_cast< eBlendMode >( i ), Alpha_Normal, 0.75f, policy_mono_chunk, 1, &event_copy, &event_blend0 );
+        ctx.Blend( blockShade, blockCanvas, srcRect, FVec2I( x, y + srcRect.h - shadeH ), Blend_Normal, Alpha_Normal, 0.5f, policy_mono_chunk, 1, &event_blend0, &event_blend1 );
+    }
+    ctx.Finish();
+
+    // We process the text in a separate loop because we have a concurrency constraint on the FFont object.
+    for( int i = 0; i < NumBlendingModes; ++i ) {
+        int x = ( i % 8 ) * srcRect.w;
+        int y = ( i / 8 ) * srcRect.h;
+        std::string bm = kwBlendMode[i];
         typedef std::codecvt_utf8<wchar_t> convert_type;
         std::wstring_convert<convert_type, wchar_t> converter;
         std::wstring wbm = converter.from_bytes(bm);
-        RenderText( threadPool, ULIS_BLOCKING, perfIntentText, host, ULIS_NOCB, blockCanvas, wbm, font, 16, white, FTransformation2D::MakeTranslationTransform( x + 4, 4 + y + srcRect.h - shadeH ), ULIS_NOAA );
+        ctx.RasterText( blockCanvas, wbm, font, 16, FMat3F::MakeTranslationMatrix( x + 4, y + srcRect.h - 4 ), white, policy_mono_chunk );
+        ctx.Finish();
     }
-
-    Fence( *threadPool );
-    delete  blockBase;
-    delete  blockOver;
 
     QApplication    app( argc, argv );
     QWidget*        widget  = new QWidget();
-    QImage*         image   = new QImage( blockCanvas->Bits()
-                                        , blockCanvas->Width()
-                                        , blockCanvas->Height()
-                                        , blockCanvas->BytesPerScanLine()
+    QImage*         image   = new QImage( blockCanvas.Bits()
+                                        , blockCanvas.Width()
+                                        , blockCanvas.Height()
+                                        , blockCanvas.BytesPerScanLine()
                                         , QImage::Format_RGBA8888 );
     QPixmap         pixmap  = QPixmap::fromImage( *image );
     QLabel*         label   = new QLabel( widget );
@@ -91,10 +112,6 @@ main( int argc, char *argv[] ) {
     delete  label;
     delete  image;
     delete  widget;
-
-    delete  blockCanvas;
-    delete  blockShade;
-    XDeleteThreadPool( threadPool );
 
     return  exit_code;
 }
