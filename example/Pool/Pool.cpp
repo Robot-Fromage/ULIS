126ec3f190af6b9d43beee6592132190c546c80f
Clement-Praxinos <clement.berthaud@praxinos.coop>
2021-03-02 10:31:38 +0100

    feat: add example pool to test issues

---
+++ example/Pool/Pool.cpp
@@ -0,0 +1,39 @@
+// IDDN FR.001.250001.004.S.X.2019.000.00000
+// ULIS is subject to copyright laws and is the legal and intellectual property of Praxinos,Inc
+/*
+*   ULIS
+*__________________
+* @file         Test.cpp
+* @author       Clement Berthaud
+* @brief        Test application for ULIS3.
+* @copyright    Copyright 2018-2021 Praxinos, Inc. All Rights Reserved.
+* @license      Please refer to LICENSE.md
+*/
+#include <ULIS>
+using namespace ::ULIS;
+
+int
+main(int argc,char *argv[]) 
+{
+    FThreadPool pool;
+    FCommandQueue queue( pool );
+    eFormat fmt = Format_LabAF;
+    FContext ctx( queue, fmt );
+    FHardwareMetrics hw;
+    FSchedulePolicy policy_cache_efficient( ScheduleRun_Multi, ScheduleMode_Chunks, ScheduleParameter_Length, hw.L1CacheSize() );
+    FSchedulePolicy policy_mono_chunk( ScheduleRun_Mono, ScheduleMode_Chunks, ScheduleParameter_Count, 1 );
+
+    //Data
+    FBlock canvas( 1024, 1024, fmt );
+
+    int repeat = 500;
+    TArray< FEvent > events( repeat );
+    ctx.Clear( canvas, canvas.Rect(), policy_cache_efficient, 0, nullptr, &events[0] );
+    for( int i = 1; i < repeat; ++i ) {
+        ctx.Clear( canvas, canvas.Rect(), policy_cache_efficient, 1, &events[i-1], &events[i] );
+        ctx.Flush();
+    }
+    ctx.Fence();
+
+    return  0;
+}
