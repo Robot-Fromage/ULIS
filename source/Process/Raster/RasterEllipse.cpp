54c75d2615708427a60fc3b890e9557d4dbe5862
Clement-Praxinos <clement.berthaud@praxinos.coop>
2021-04-20 14:15:07 +0200

    fix: include qualified path in Raster Ellipse

--- source/Process/Raster/RasterEllipse.cpp
+++ source/Process/Raster/RasterEllipse.cpp
@@ -10,8 +10,8 @@
 * @license      Please refer to LICENSE.md
 */
 
-#include "RasterEllipse.h"
-#include "RasterLine.h"
+#include "Process/Raster/RasterEllipse.h"
+#include "Process/Raster/RasterLine.h"
 
 ULIS_NAMESPACE_BEGIN
 
