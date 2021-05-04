5e85048fa2d98358ed56eb0916f5e857c40aa21b
Clement-Praxinos <clement.berthaud@praxinos.coop>
2021-05-04 13:18:41 +0200

    fix: prepare all files for pyULISInteractive

---
+++ binding/Python/interactive/src/Console.cpp
@@ -0,0 +1,102 @@
+// IDDN FR.001.250001.004.S.X.2019.000.00000
+// ULIS is subject to copyright laws and is the legal and intellectual property of Praxinos,Inc
+/*
+*   ULIS
+*__________________
+* @file         Canvas.cpp
+* @author       Clement Berthaud
+* @brief        pyULIS_Interactive application for testing pyULIS.
+* @copyright    Copyright 2018-2021 Praxinos, Inc. All Rights Reserved.
+* @license      Please refer to LICENSE.md
+*/
+#include "Canvas.h"
+#include <QImage>
+#include <QLabel>
+#include <QPixmap>
+#include <QTimer>
+#include <QEvent>
+#include <QKeyEvent>
+#include <QMouseEvent>
+
+SCanvas::~SCanvas() {
+    delete  mTimer;
+    delete  mImage;
+    delete  mPixmap;
+    delete  mLabel;
+}
+
+SCanvas::SCanvas( FULISLoader& iHandle )
+    : QWidget( nullptr )
+    , mHandle( iHandle )
+    , mCanvas( FBlock( 800, 600, mHandle.Format() ) )
+    , mImage( nullptr )
+    , mPixmap( nullptr )
+    , mLabel( nullptr )
+    , mTimer( nullptr )
+{
+    setFixedSize( 800, 600 );
+
+    FContext& ctx = mHandle.Context();
+    ctx.Fill( mCanvas, FColor::White );
+    ctx.Finish();
+
+    mImage  = new QImage( mCanvas.Bits(), mCanvas.Width(), mCanvas.Height(), mCanvas.BytesPerScanLine(), QImage::Format::Format_RGBA8888 );
+    mPixmap = new QPixmap( QPixmap::fromImage( *mImage ) );
+    mLabel  = new QLabel( this );
+    mLabel->setPixmap( *mPixmap );
+    this->QWidget::setFixedSize( mPixmap->size() );
+
+    mTimer = new QTimer();
+    mTimer->setInterval( 1000.0 / 24.0 );
+    QObject::connect( mTimer, SIGNAL( timeout() ), this, SLOT( tickEvent() ) );
+    mTimer->start();
+
+    py::module_ pyULIS4 = py::module_::import("pyULIS4");
+    py::exec( R"(
+        from pyULIS4 import *
+        canvas = FBlock( 800, 600, Format_RGBA8 )
+    )" );
+
+    py::module_ main = py::module_::import("__main__");
+    main.add_object( "canvas2", py::cast( &mCanvas ), true );
+
+    // Sample backward / forward
+    // py::object obj = py::cast( &mCanvas );
+    //FBlock* canvas = pyCanvas.cast< FBlock* >();
+    //std::cout << canvas->Format();
+    //std::cout << canvas->Rect().w;
+    //auto dummy = 0;
+}
+
+void
+SCanvas::mouseMoveEvent( QMouseEvent* event ) {
+}
+
+void
+SCanvas::keyPressEvent( QKeyEvent* event ) {
+}
+
+void
+SCanvas::tickEvent() {
+    FContext& ctx = mHandle.Context();
+    ctx.Fill( mCanvas, FColor::White );
+    ctx.Finish();
+    //py::object pyCanvas = pyULIS4.attr( "FBlock" )( 800, 600, Format_RGBA8 );
+
+    auto message = "Hello world from python embed !"_s;
+
+    try {
+        py::exec(R"(
+            print( "block from py", canvas2.Width() )
+        )");
+    } catch( const std::exception& ) {
+        std::cout << "error" << std::endl;
+    }
+
+    py::module_ main = py::module_::import("__main__");
+    py::object canvas_attr = main.attr( "canvas" );
+    FBlock* canvas = canvas_attr.cast< FBlock* >();
+    std::cout << "block from cpp" << canvas->Width() << std::endl;
+    auto dummy = 0;
+}
+
---
+++ binding/Python/interactive/src/Viewport.h
@@ -0,0 +1,52 @@
+// IDDN FR.001.250001.004.S.X.2019.000.00000
+// ULIS is subject to copyright laws and is the legal and intellectual property of Praxinos,Inc
+/*
+*   ULIS
+*__________________
+* @file         Canvas.h
+* @author       Clement Berthaud
+* @brief        pyULIS_Interactive application for testing pyULIS.
+* @copyright    Copyright 2018-2021 Praxinos, Inc. All Rights Reserved.
+* @license      Please refer to LICENSE.md
+*/
+#include "ULISLoader.h"
+#include <ULIS>
+
+#include <pybind11/embed.h>
+namespace py = pybind11;
+using namespace py::literals;
+
+#include <QWidget>
+
+class QImage;
+class QLabel;
+class QPixmap;
+class QTimer;
+
+using namespace ::ULIS;
+
+class SCanvas : public QWidget
+{
+    Q_OBJECT
+
+public:
+    ~SCanvas();
+    SCanvas( FULISLoader& iHandle );
+
+protected:
+    virtual void mouseMoveEvent( QMouseEvent* event ) override;
+    virtual void keyPressEvent( QKeyEvent* event ) override;
+
+private:
+    Q_SLOT void tickEvent();
+
+private:
+    FULISLoader& mHandle;
+    FBlock mCanvas;
+    QImage* mImage;
+    QPixmap* mPixmap;
+    QLabel* mLabel;
+    QTimer* mTimer;
+    py::scoped_interpreter mGuard;
+};
+
