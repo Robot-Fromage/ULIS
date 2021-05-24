8c4a3bfe385a92889b60ede7366a7e8387386004
Clement-Praxinos <clement.berthaud@praxinos.coop>
2021-05-24 10:57:58 +0200

    feat: update demo.js

--- binding/WebAssembly/module/demo/demo.js
+++ binding/WebAssembly/module/demo/demo.js
@@ -6,35 +6,24 @@
 // Have fun,
 // - Layl.
 var elapsed = 0.0;
+var pool = new wULIS4.FThreadPool( 1 );
+var queue = new wULIS4.FCommandQueue( pool );
+var fmt = wULIS4.eFormat.Format_RGBA8;
+var ctx = new wULIS4.FContext( queue, fmt, wULIS4.ePerformanceIntent.PerformanceIntent_Max );
+var w = 512;
+var h = 512;
+var block = new wULIS4.FBlock( w, h, fmt );
 
 onStart = function() {
     stdout( wULIS4.FullLibraryInformationString() );
-    stdout( wULIS4.FThreadPool.MaxWorkers() );
-    var pool = new wULIS4.FThreadPool( 1 );
-    pool.delete();
 }
 
 onUpdate = function( delta ) {
     elapsed += delta;
-    var block = new wULIS4.FBlock(
-          512
-        , 512
-        , wULIS4.eFormat.Format_RGBA8
-        , null
-        , new wULIS4.FOnInvalidBlock()
-        , wULIS4.FOnCleanupData.OnCleanupFreeMemory()
-    );
-
-    var color = wULIS4.FColor.HSVA8( ( elapsed * 80 ) % 255, 255, 255, 255 );
-    for( let y = 0; y < block.Height(); y++ ) {
-        for( let x = 0; x < block.Width(); x++ ) {
-            block.SetPixel( x, y, color );
-        }
-    }
-
+    var color = wULIS4.FColor.HSVA8( ( elapsed * 40 ) % 255, 255, 255, 255 );
+    ctx.Fill( block, color, block.Rect() );
+    ctx.Finish();
     refresh( block );
-
     color.delete();
-    block.delete();
 }
 
