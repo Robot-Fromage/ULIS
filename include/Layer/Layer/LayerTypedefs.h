c8a8e1c59ba0410b1516474da60c666033683be5
Clement-Praxinos <clement.berthaud@praxinos.coop>
2021-06-11 13:31:09 +0200

    feat: add LayerTypedefs files for easy typedefs of default provided and DLL Exported Layer types using FBlock as BlockType

---
+++ include/Layer/Layer/LayerTypedefs.h
@@ -0,0 +1,34 @@
+// IDDN FR.001.250001.004.S.X.2019.000.00000
+// ULIS is subject to copyright laws and is the legal and intellectual property of Praxinos,Inc
+/*
+*   ULIS
+*__________________
+* @file         LayerTypes.h
+* @author       Clement Berthaud
+* @brief        This file provides the declaration of specific Layer Types
+* @copyright    Copyright 2018-2021 Praxinos, Inc. All Rights Reserved.
+* @license      Please refer to LICENSE.md
+*/
+#pragma once
+#include "Core/Core.h"
+#include "Layer/Layer/Layer.h"
+#include "Layer/Layer/LayerFolder.h"
+#include "Layer/Layer/LayerImage.h"
+#include "Layer/Layer/LayerStack.h"
+#include "Layer/Layer/LayerText.h"
+
+ULIS_NAMESPACE_BEGIN
+// Typedefs
+typedef TLayerFolder< FBlock, void, void, void > FLayerFolder;
+typedef TLayerImage< FBlock, void, void, void > FLayerImage;
+typedef TLayerStack< FBlock, void, void, void > FLayerStack;
+typedef TLayerText< FBlock, void, void, void > FLayerText;
+
+// Exports
+template class ULIS_API TLayerFolder< FBlock, void, void, void >;
+template class ULIS_API TLayerImage< FBlock, void, void, void >;
+template class ULIS_API TLayerStack< FBlock, void, void, void >;
+template class ULIS_API TLayerText< FBlock, void, void, void >;
+
+ULIS_NAMESPACE_END
+
